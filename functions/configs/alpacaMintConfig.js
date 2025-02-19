35367be379309c54cdc1f5aed94a3eef8514b775
Aayush Gupta <guptaaayush432@gmail.com>
2025-02-19 16:38:47 +0530

    updated alpacaMintConfig.js

--- functions/configs/alpacaMintConfig.js
+++ functions/configs/alpacaMintConfig.js
@@ -2,24 +2,26 @@
 // this is the config file for the chainlink functions 
 // which tell our simulator how to work
 
-const fs = require("fs");
-const {
-  Location,
-  ReturnType,
-  CodeLanguage,
-} = require("@chainlink/functions-toolkit");
+const fs = require("fs")
+const { Location, ReturnType, CodeLanguage } = require("@chainlink/functions-toolkit")
 
+// Configure the request by setting the fields below
 const requestConfig = {
+  // String containing the source code to be executed
   source: fs.readFileSync("./functions/sources/alpacaBalance.js").toString(),
+  //source: fs.readFileSync("./API-request-example.js").toString(),
+  // Location of source code (only Inline is currently supported)
   codeLocation: Location.Inline,
-  secrets: {
-    alpacaKey: process.env.ALPACA_KEY,
-    alpacaSecret: process.env.ALPACA_SECRET,
-  },
-  secretsLocation: Location.DONHosted, // this is where we store our secrets in our DON
+  // Optional. Secrets can be accessed within the source code with `secrets.varName` (ie: secrets.apiKey). The secrets object can only contain string values.
+  secrets: { alpacaKey: process.env.ALPACA_KEY ?? "", alpacaSecret: process.env.ALPACA_SECRET ?? "" },
+  // Optional if secrets are expected in the sourceLocation of secrets (only Remote or DONHosted is supported)
+  secretsLocation: Location.DONHosted,
+  // Args (string only array) can be accessed within the source code with `args[index]` (ie: args[0]).
   args: [],
+  // Code language (only JavaScript is currently supported)
   codeLanguage: CodeLanguage.JavaScript,
+  // Expected type of the returned value
   expectedReturnType: ReturnType.uint256,
-};
+}
 
-module.exports = requestConfig;
+module.exports = requestConfig
